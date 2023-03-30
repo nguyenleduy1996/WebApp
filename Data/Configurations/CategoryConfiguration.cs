be410d22b195dce44ea5c3dd8f67081b03ae2403
Nguyen Le <nguyenleduy1996@gmail.com>
2023-03-30 22:44:01 +0700

    no message

---
+++ Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Enums;
+using DataLayer.ModelDB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+
+            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+        }
+    }
+}
