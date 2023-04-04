4e50273fdcdc33f453f08d8aba09b19d12a1ce8e
Nguyen Le <nguyenleduy1996@gmail.com>
2023-04-04 20:24:31 +0700

    no message

--- Data/DataContext/MyDbContext.cs
+++ Data/DataContext/MyDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using DataLayer.Configurations;
 using DataLayer.Extensions;
 using DataLayer.ModelDB;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace MyWebApiApp.Data
 {
-    public class MyDbContext: DbContext
+    public class MyDbContext: IdentityDbContext
     {
         public MyDbContext(DbContextOptions options) : base(options) 
         { 
@@ -29,17 +32,50 @@ namespace MyWebApiApp.Data
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
 
-           // modelBuilder.ApplyConfiguration(new AppUserConfiguration());
-           // modelBuilder.ApplyConfiguration(new AppRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+            modelBuilder.ApplyConfiguration(new AppRoleConfiguration());
             modelBuilder.ApplyConfiguration(new ProductImageConfiguration());
             modelBuilder.ApplyConfiguration(new SlideConfiguration());
 
+            //
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x => new { x.UserId, x.RoleId });
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("AppUserLogins").HasKey(x => x.UserId);
+
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => x.UserId);
+
             modelBuilder.Seed();
+            base.OnModelCreating(modelBuilder);
         }
 
         #region DbSet
+        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products  { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        public DbSet<Slide> Slides { get; set; }
         #endregion
     }
 }
--- Data/ModelDB/Order.cs
+++ Data/ModelDB/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,11 @@ namespace DataLayer.ModelDB
         public string ShipAddress { set; get; }
         public string ShipEmail { set; get; }
         public string ShipPhoneNumber { set; get; }
-      //  public OrderStatus Status { set; get; }
+         public OrderStatus Status { set; get; }
 
         public List<OrderDetail> OrderDetails { get; set; }
 
-       // public AppUser AppUser { get; set; }
+        public AppUser AppUser { get; set; }
 
 
     }
