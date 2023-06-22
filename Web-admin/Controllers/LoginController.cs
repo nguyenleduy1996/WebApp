2602162512af1d6219c933ffdea7317dccae9289
Nguyen Le <nguyenleduy1996@gmail.com>
2023-06-22 23:52:07 +0700

    no message

--- Web-admin/Controllers/LoginController.cs
+++ Web-admin/Controllers/LoginController.cs
@@ -41,7 +41,11 @@ namespace Web_admin.Controllers
                 return View(ModelState);
 
             var result = await _userApiClient.Authenticate(request);
-
+            if (result.ResultObj == null)
+            {
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
             var userPrincipal = this.ValidateToken(result.ResultObj);
             var authProperties = new AuthenticationProperties
             {
